3d7df34a4aa64d10d3c6c3e56560530807e50cba
FrancescoBenassi <francybenna77@gmail.com>
2024-11-28 18:09:06 +0100

    feat Prodotto: fixed and added discount method

--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Prodotto {
     private int code;
     private String name;
@@ -5,56 +7,61 @@ public class Prodotto {
     private float price;
     private float iva;
 
-    public Prodotto(int code, String name, String brand, float price, float iva){
-        this.code = code;
+    public Prodotto(String name, String brand, float price, float iva) {
+        Random rand = new Random();
+        this.code = rand.nextInt(999999);
         this.name = name;
         this.brand = brand;
         this.price = price;
         this.iva = iva;
     }
 
-    public int getCode(){
+    public int getCode() {
         return this.code;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getBrand(){
+    public String getBrand() {
         return this.brand;
     }
 
-    public void setBrand(String brand){
+    public void setBrand(String brand) {
         this.brand = brand;
     }
 
-    public float getPrice(){
+    public float getPrice() {
         return this.price;
     }
 
-    public void setPrice(float price){
+    public void setPrice(float price) {
         this.price = price;
     }
 
-    public float getIva(){
+    public float getIva() {
         return this.iva;
     }
 
-    public void setIva(float iva){
+    public void setIva(float iva) {
         this.iva = iva;
     }
 
-    public String getPriceIva(){
-        return "Il prezzo inclusa l'iva è di " + (this.price + (this.price / 100 * this.iva)) + "€";
+    public float getBasicPrice() {
+        return  this.price;
     }
-    
-    public String getBasicPrice(){
-        return "Il prezzo base esclusa l'iva è di " + this.price + "€";
+
+    public float getPriceIva() {
+        return this.price = this.price + (this.price / 100 * this.iva);
+    }
+
+    public float getDiscountPrice(){
+            return this.price = this.price - (this.price * 2 / 100);
     }
 
 }
