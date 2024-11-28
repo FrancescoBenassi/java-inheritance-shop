901e978daf2b38eb846c4685e671ea2d6039bb2d
FrancescoBenassi <francybenna77@gmail.com>
2024-11-28 18:11:54 +0100

    feat Smartphone: fixed and added discountSmartphone method

--- src/Smartphone.java
+++ src/Smartphone.java
@@ -2,25 +2,29 @@ public class Smartphone extends Prodotto {
     private int codeImei;
     private int memory;
 
-    public Smartphone(int code, String name, String brand, float price, float iva, int codeImei, int memory){
-        super(code, name, brand, price, iva);
+    public Smartphone(String name, String brand, float price, float iva, int codeImei, int memory) {
+        super(name, brand, price, iva);
         this.codeImei = codeImei;
         this.memory = memory;
     }
 
-    public int getCodeImei(){
+    public int getCodeImei() {
         return this.codeImei;
     }
 
-    public void setCodeImei(int codeImei){
+    public void setCodeImei(int codeImei) {
         this.codeImei = codeImei;
     }
 
-    public int getMemory(){
+    public int getMemory() {
         return this.memory;
     }
 
-    public void setMemory(int memory){
+    public void setMemory(int memory) {
         this.memory = memory;
+    }
+
+    public float getDiscountSmartphone() {
+        return super.getPrice() - (super.getPrice() * 5 / 100);
     }
 }
